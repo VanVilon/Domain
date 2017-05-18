45df684e94ba1134b87b2dee0cfe3ccf134e5069
Unknown <mateusz@theministryofideas.com>
2017-05-18 21:54:58 +0200

    StoredEvent tests cd.

--- Domain.Core.UnitTests/Events/Sourcing/StoredEventTests.cs
+++ Domain.Core.UnitTests/Events/Sourcing/StoredEventTests.cs
@@ -21,6 +21,24 @@ namespace Domain.Model.UnitTests.Events.Sourcing
             Assert.IsInstanceOfType(domainEvent, typeof(TestEvent));
             Assert.AreEqual("lol", domainEvent.SomeimportantProperty);
         }
+
+        [TestMethod]
+        public void Stored_event_is_equatable_to_other_stored_event_with_same_data()
+        {
+            var se1 = new StoredEvent(typeof(TestEvent), new EventBody("a"));
+            var se2 = new StoredEvent(typeof(TestEvent), new EventBody("a"));
+
+            Assert.AreEqual(se1, se2);
+        }
+
+        [TestMethod]
+        public void Stored_event_is_not_equatable_to_other_stored_event_with_different_data()
+        {
+            var se1 = new StoredEvent(typeof(TestEvent), new EventBody("a"));
+            var se2 = new StoredEvent(typeof(TestEvent), new EventBody("b"));
+
+            Assert.AreNotEqual(se1, se2);
+        }
     }
 
     public class TestEvent : IDomainEvent
