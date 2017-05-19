03dc10b447e1b326822bb54e6a6ae313d69f7a45
Mateusz <mateusz@theministryofideas.com>
2017-05-19 12:04:17 +0200

    Aggregate contract and base class

---
+++ Domain.Infrastructure/AggregateBase.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using Domain.Infrastructure.Events;
+using Microsoft.CSharp.RuntimeBinder;
+
+namespace Domain.Infrastructure
+{
+    public abstract class AggregateBase : IAggregate
+    {
+        private Dictionary<Type, Action<IDomainEvent>> _routes;
+        private readonly List<IDomainEvent> _uncommitedEvents;
+        public Guid Id { get; }
+        public int Version { get; protected set; }
+
+        public IEnumerable<IDomainEvent> UncommitedEvents => _uncommitedEvents;
+
+        protected AggregateBase()
+        {
+            _uncommitedEvents = new List<IDomainEvent>();
+        }
+
+        public void ApplyEvent(IDomainEvent @event)
+        {
+            try
+            {
+                ((dynamic)this).Apply(@event);
+                Version++;
+            }
+            catch (RuntimeBinderException)
+            {
+            }
+        }
+
+        public void ClearUncommitedEvents()
+        {
+            _uncommitedEvents.Clear();
+        }
+
+        public void Recreate(List<IDomainEvent> events)
+        {
+            foreach (var @event in events)
+            {
+                ApplyEvent(@event);
+            }
+        }
+    }
+}
---
+++ Domain.Infrastructure/IAggregate.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using Domain.Infrastructure.Events;
+
+namespace Domain.Infrastructure
+{
+    public interface IAggregate
+    {
+        Guid Id { get; }
+        int Version { get; }
+        IEnumerable<IDomainEvent> UncommitedEvents { get; }
+
+        void ApplyEvent(IDomainEvent @event);
+        void ClearUncommitedEvents();
+        void Recreate(List<IDomainEvent> events);
+    }
+}
