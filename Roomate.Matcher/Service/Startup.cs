d0cc28e9ba940895f71fd8ee26a0df4a34826a4d
Unknown <mateusz@theministryofideas.com>
2017-05-31 21:52:11 +0200

    ProfilesMatcher infrastructure

--- Roomate.Matcher/Service/Startup.cs
+++ Roomate.Matcher/Service/Startup.cs
@@ -8,7 +8,7 @@ using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Logging;
 
-namespace Roomate.Matcher.Service
+namespace Service
 {
     public class Startup
     {
